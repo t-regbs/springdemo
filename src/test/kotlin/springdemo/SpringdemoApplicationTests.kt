78c4e4a762fe347a8dc043644691fdd76d3350bf
Timilehin <aregbestimi@gmail.com>
2022-02-11 16:21:03 +0100

    Initial commit

---
+++ src/test/kotlin/springdemo/SpringdemoApplicationTests.kt
@@ -0,0 +1,12 @@
+package springdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
